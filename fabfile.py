22ea7882195f48b1868cea54b61a1d60b2f36d1a
Woody Huang <woody1573@163.com>
2018-12-26 11:03:05 +0800

    add fabric tool to automatically install on raspberry pi
    Signed-off-by: Woody Huang <woody1573@163.com>

---
+++ fabfile.py
@@ -0,0 +1,16 @@
+import re
+from fabric.api import run, env
+from fabric.contrib.project import rsync_project
+
+__author__ = "Woody Huang"
+__version__ = "1.0"
+
+env.hosts = [
+    "pi@192.168.199.153"
+]
+
+def install():
+    with open("./.gitignore") as f:
+        excludes = re.sub('\n+', "\n", f.read()).split("\n")
+    excludes.append(".git")
+    rsync_project("/home/pi/player/", "./", exclude=excludes)
